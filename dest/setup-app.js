faaa8c361e7ff25f44842836eaa5a76af27a1ac2
Alex <steblyakov.aleksandr@gmail.com>
2025-04-05 18:51:08 +0300

    Basic CRUD functionality added

--- dest/setup-app.js
+++ dest/setup-app.js
@@ -5,11 +5,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.setupApp = void 0;
 const express_1 = __importDefault(require("express"));
+const mock_data_1 = require("./db/mock-data");
+const driver_types_1 = require("./drivers/driver-types");
 const setupApp = (app) => {
     app.use(express_1.default.json()); // middleware для парсинга JSON в теле запроса
     // основной роут
     app.get("/", (req, res) => {
         res.status(200).send("Hello my first BACK-END APP!");
+    });
+    app.get("/drivers", (req, res) => {
+        res.status(200).json(mock_data_1.driversDb.drivers);
+    });
+    app.get('/drivers/:id', (req, res) => {
+        const driver = mock_data_1.driversDb.drivers.find((d) => d.id === +req.params.id);
+        if (!driver) {
+            res.status(404).send({ message: "Driver not found" });
+            return;
+        }
+        res.status(200).send(driver);
+    });
+    app.post('/drivers', (req, res) => {
+        const newDriver = Object.assign({ id: mock_data_1.driversDb.drivers.length ? mock_data_1.driversDb.drivers[mock_data_1.driversDb.drivers.length - 1].id + 1 : 1, status: driver_types_1.DriverStatus.Online, created: new Date() }, req.body);
+        mock_data_1.driversDb.drivers.push(newDriver);
+        res.status(201).send(newDriver);
+    });
+    app.delete('/testing/all-data', (req, res) => {
+        mock_data_1.driversDb.drivers = [];
+        res.sendStatus(204);
     });
     return app;
 };
--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,4 +1,6 @@
-import express, { Express } from "express";
+import express, {Express, Request, Response} from "express";
+import {driversDb} from "./db/mock-data";
+import {Driver, DriverStatus} from "./drivers/driver-types";
 
 export const setupApp = (app: Express) => {
     app.use(express.json()); // middleware для парсинга JSON в теле запроса
@@ -7,5 +9,40 @@ export const setupApp = (app: Express) => {
     app.get("/", (req, res) => {
         res.status(200).send("Hello my first BACK-END APP!");
     });
+
+    app.get("/drivers", (req, res) => {
+        res.status(200).json(driversDb.drivers);
+    });
+
+    app.get('/drivers/:id', (req: Request, res: Response) => {
+        const driver = driversDb.drivers.find((d) => d.id === +req.params.id);
+        if(!driver) {
+            res.status(404).send({ message: "Driver not found" });
+            return;
+        }
+
+        res.status(200).send(driver);
+    });
+
+
+    app.post('/drivers', (req: Request, res: Response) => {
+        const newDriver: Driver = {
+            id: driversDb.drivers.length ? driversDb.drivers[driversDb.drivers.length - 1].id + 1 : 1,
+            status: DriverStatus.Online,
+            created: new Date(),
+            ...req.body
+        };
+
+        driversDb.drivers.push(newDriver);
+
+        res.status(201).send(newDriver);
+    });
+
+    app.delete('/testing/all-data', (req: Request, res: Response) => {
+        driversDb.drivers = [];
+        res.sendStatus(204);
+    });
+    
     return app;
 };
+
