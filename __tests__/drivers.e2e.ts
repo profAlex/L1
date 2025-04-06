d72ef2060bbc49a411edc2e13b2e1d9e8db75f73
Alex <steblyakov.aleksandr@gmail.com>
2025-04-06 17:29:30 +0300

    Woking on e2e testing with Jest and Supertest

--- __tests__/drivers.e2e.ts
+++ __tests__/drivers.e2e.ts
@@ -1,11 +1,18 @@
-import request from "supertest";
-import express from "express";
-import { app } from "../src"
+import request = require('supertest');
+//import request from 'supertest';
 
-describe("GET /", () => {
-    it("should respond with a 200", async() => {
+//import express from "express";
+import { app } from "../src"
+import { HttpStatus } from "../src/core/http-statuses";
+
+describe("Test API", () => {
+
+    it("GET / - should respond with a 200", async() => {
         const res = await request(app).get("/");
-        expect(res.status).toBe(200);
+        expect(res.status).toBe(HttpStatus.Ok);
         expect(res.text).toBe("Hello my first BACK-END APP!");
     });
+
+    it("GET /drivers")
+
 });
